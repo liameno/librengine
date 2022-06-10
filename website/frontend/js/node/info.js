eda2d1963fb13189e560ded6f970bc96ff5fdd09
liameno <rhCWZ765hg@protonmail.com>
2022-06-10 14:55:34 +0300

    Edit chart type (/node/info)

--- website/frontend/js/node/info.js
+++ website/frontend/js/node/info.js
@@ -22,32 +22,28 @@ function chart() {
 
     const ctx = document.getElementById('chart').getContext('2d');
     const chart = new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
             labels: labels,
             datasets: [{
                 label: '#added websites by days',
                 data: values,
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    '#70c17c',
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    '#7DE38D',
                 ],
-                borderWidth: 1
+                borderWidth: 1,
+                //fill: 'start'
             }]
         },
         options: {
+            elements: {
+              line: {
+                  //tension: 0.2
+              }
+            },
             scales: {
                 y: {
                     beginAtZero: true,
